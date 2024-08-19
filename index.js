0fe78e345b7d65977b2545c72de0d6b46d5426a8
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2024-08-19 18:32:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-const drink = prompt("Будь ласка, виберіть 'Кава', 'Чай' або 'Сік'");
-console.log("Ви обрали:", drink);
+const drink = prompt("Оберіть 'Кава', 'Чай', 'Сік'");
+console.log("Ваш вибір:", drink);
 
-const day = prompt("Будь ласка, введіть день тижня");
+const day = prompt("Оберіть день тижня");
 switch (day.toLowerCase()) {
   case "понеділок":
   case "вівторок":
   case "середа":
   case "четвер":
   case "п'ятниця":
-    console.log(day, "це робочий день");
+    console.log(day, "робочий день");
     break;
   case "субота":
   case "неділя":
-    console.log(day, "це вихідний день");
+    console.log(day, "вихідний день");
     break;
   default:
-    console.log(day, "це не день тижня");
+    console.log(day, "не день тижня");
 }
 
-const month = prompt("Будь ласка, введіть місяць");
+const month = prompt("Виберіть місяць");
 switch (month.toLowerCase()) {
   case "березень":
   case "квітень":
   case "травень":
-    console.log(month, "відноситься до весни");
+    console.log(month, "це весна");
     break;
   case "червень":
   case "липень":
   case "серпень":
-    console.log(month, "відноситься до літа");
+    console.log(month, "це літо");
     break;
   case "вересень":
   case "жовтень":
   case "листопад":
-    console.log(month, "відноситься до осені");
+    console.log(month, "це осень");
     break;
   case "грудень":
   case "січень":
   case "лютий":
-    console.log(month, "відноситься до зими");
+    console.log(month, "це зима");
     break;
   default:
-    console.log(month, "це не місяць");
+    console.log(month, "не місяць");
 }
 
-const month2 = prompt("Будь ласка, введіть місяць");
+const month2 = prompt("Оберіть місяць");
 switch (month2.toLowerCase()) {
   case "квітень":
   case "червень":
   case "вересень":
   case "листопад":
-    console.log(month2, "має 30 днів");
+    console.log(month2, "30 днів");
     break;
   case "березень":
   case "травень":
@@ -59,16 +59,16 @@ switch (month2.toLowerCase()) {
   case "жовтень":
   case "грудень":
   case "січень":
-    console.log(month2, "має 31 день");
+    console.log(month2, "31 день");
     break;
   case "лютий":
-    console.log("У 2024 році, ", month2, "має 29 днів");
+    console.log("В цьому році, ", month2, "має 29 днів");
     break;
   default:
-    console.log(month2, "це не місяць");
+    console.log(month2, "не місяць");
 }
 
-const color = prompt("Будь ласка, введіть колір (бажано червоний, жовтий або зелений)");
+const color = prompt("Оберіть колір (червоний, жовтий, зелений)");
 let action;
 switch (color.toLowerCase()) {
   case "червоний":
@@ -87,7 +87,7 @@ console.log(action);
 
 const number1 = Number.parseFloat(prompt("Виберіть перше число"));
 const number2 = Number.parseFloat(prompt("Виберіть друге число"));
-const operation = prompt("Виберіть дію (+, -, *, /)");
+const operation = prompt("Виберіть дію (+, -, *, /)(ви можете поділили на 0)");
 let result;
 switch (operation) {
   case "+":
@@ -103,6 +103,6 @@ switch (operation) {
     result = number1 * number2;
     break;
     default:
-        result = "Неправильна дія";
+        result = "Некорректна дія";
 }
 console.log(result);
